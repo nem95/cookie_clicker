87bee7585b242e72e6048f2dee9b8695fe3d65a9
Naïm <naiim.chatti@gmail.com>
2018-07-26 23:14:23 +0200

    Auto stash before revert of "Merge branch 'make-autocookie-work'"

---
+++ tests/unit/Cookie.spec.js
@@ -0,0 +1,26 @@
+import { shallowMount } from "@vue/test-utils";
+// import Vuex from "vuex";
+import Cookie from "@/components/Cookie";
+import store from "../../src/store";
+
+describe("Cookie.vue", () => {
+  let Store;
+
+  beforeEach(() => {
+    // const localStorageMock = {
+    //     getItem: jest.fn(),
+    //     setItem: jest.fn(),
+    //     clear: jest.fn()
+    // };
+    // global.localStorage = localStorageMock;
+    localStorage.clear();
+    Store = store;
+  });
+
+  it("Change the counter value whene store cookieCounter Change", () => {
+    const wrapper = shallowMount(Cookie, { store });
+    const counter = wrapper.find("#counter");
+    //console.log(Store);
+    expect(counter.element.value).toMatch(Store.store.cookieCounter);
+  });
+});
